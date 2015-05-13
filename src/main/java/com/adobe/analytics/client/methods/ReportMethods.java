fa3d061268cec23d202b82a82a43f629b799a947
Tomasz Rękawek <trekawek@gmail.com>
2015-05-13 16:36:50 +0200

    Fixed parsing of the results in ReportMethods.

--- src/main/java/com/adobe/analytics/client/methods/ReportMethods.java
+++ src/main/java/com/adobe/analytics/client/methods/ReportMethods.java
@@ -1,10 +1,13 @@
 package com.adobe.analytics.client.methods;
 
+import static com.adobe.analytics.client.JsonUtil.m;
+
 import java.io.IOException;
 
 import com.adobe.analytics.client.AnalyticsClient;
 import com.adobe.analytics.client.domain.ReportDescription;
 import com.adobe.analytics.client.domain.ReportResponse;
+import com.google.gson.JsonObject;
 
 public class ReportMethods {
 
@@ -15,10 +18,12 @@ public class ReportMethods {
 	}
 
 	public int queue(ReportDescription reportDesc) throws IOException {
-		return client.callMethod("Report.Queue", reportDesc, Integer.class);
+		final JsonObject response = client.callMethod("Report.Queue", m("reportDescription", reportDesc),
+				JsonObject.class);
+		return response.get("reportID").getAsInt();
 	}
 
 	public ReportResponse get(int reportId) throws IOException {
-		return client.callMethod("Report.Get", reportId, ReportResponse.class);
+		return client.callMethod("Report.Get", m("reportID", reportId), ReportResponse.class);
 	}
 }
