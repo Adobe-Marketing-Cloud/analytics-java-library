7f5c81dc4fb71725597dec0c449111d5b79420c4
Leonardo Foderaro <startrack79@gmail.com>
2016-03-01 10:02:05 +0100

    added AnalyticClient field

--- src/main/java/com/adobe/analytics/client/domain/BookmarkFolder.java
+++ src/main/java/com/adobe/analytics/client/domain/BookmarkFolder.java
@@ -1,10 +1,14 @@
 package com.adobe.analytics.client.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.adobe.analytics.client.AnalyticsClient;
 import com.google.gson.annotations.SerializedName;
 
 public class BookmarkFolder {
+
+	private AnalyticsClient client;
 	
 	public String getName() {
 		return name;
@@ -19,6 +23,11 @@ public class BookmarkFolder {
 	}
 
 	public List<Bookmark> getBookmarks() {
+		// slightly inefficient, but it ensures the client obj availability
+		for (Bookmark bookmark : bookmarks) {
+			bookmark.setClient(this.client);
+		}
+		
 		return bookmarks;
 	}
 
@@ -34,4 +43,16 @@ public class BookmarkFolder {
 	@SerializedName("bookmarks")
 	private List<Bookmark> bookmarks;
 
+	public void setBookmarks(List<Bookmark> bookmarks) {
+		System.out.println("setting bookmark list");
+		this.bookmarks = bookmarks;
+	}
+
+	public BookmarkFolder() {
+		this.bookmarks = new ArrayList<Bookmark>();
+	}
+
+	public void setClient(AnalyticsClient client) {
+		this.client = client;
+	}
 }
