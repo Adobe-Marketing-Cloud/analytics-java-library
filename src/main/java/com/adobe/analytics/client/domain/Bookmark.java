07bd1f8c50c2c0c812c6091d476300007b7c78b2
Leonardo Foderaro <startrack79@gmail.com>
2016-03-01 10:04:46 +0100

    added getReportDescription using AnalyticsClient field

--- src/main/java/com/adobe/analytics/client/domain/Bookmark.java
+++ src/main/java/com/adobe/analytics/client/domain/Bookmark.java
@@ -1,6 +1,14 @@
 package com.adobe.analytics.client.domain;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+import com.adobe.analytics.client.AnalyticsClient;
 import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
+
+import static com.adobe.analytics.client.JsonUtil.a;
+import static com.adobe.analytics.client.JsonUtil.o;
 
 public class Bookmark {
 
@@ -25,5 +33,16 @@ public class Bookmark {
 	@SerializedName("id")
 	private long id;
 
+	private AnalyticsClient client;
 
+	public void setClient(AnalyticsClient client) {
+		this.client = client;
+	}
+	
+	public ReportDescription getReportDescription() throws IOException {
+		final Type type = new TypeToken<ReportDescriptionResponse>() {}.getType();
+		ReportDescriptionResponse rdr = client.callMethod("Bookmark.GetReportDescription", o("bookmark_id", this.id), type);
+		return rdr.getReportDescription();
+	}
+	
 }
